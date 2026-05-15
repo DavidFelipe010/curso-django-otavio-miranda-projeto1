8834df6d5f7a8f598a38156a3f2fabf90f269e45
David F. <david.dfcsb9@gmail.com>
2026-05-15 19:23:36 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name':'David F.'
     })
-
-
-def sobre(request):
-    return render(request, 'recipes/contato.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
